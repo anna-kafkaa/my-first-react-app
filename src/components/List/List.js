4391fee8288045e21a962f5c5f65447ffcdf56df
Anna Kafka <anna.kafka933@gmail.com>
2025-06-25 18:39:01 +0200

    Add support for icons in Column component via props

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,27 +1,21 @@
 import styles from './List.module.scss';
+import Column from '../Column/Column';
 
 const List = () => {
   return (
-    <div className={styles.list}>
+    <section className={styles.list}>
       <header className={styles.header}>
-        <h2 className={styles.title}>
-          Things to do <span>soon!</span>
-        </h2>
+        <h2 className={styles.title}>Things to do <span>soon!</span></h2>
       </header>
-      <p className={styles.description}>Interesting things I want to check out</p>
-      <section className={styles.columns}>
-        <article>
-          <h2>Books</h2>
-        </article>
-        <article>
-          <h2>Movies</h2>
-        </article>
-        <article>
-          <h2>Games</h2>
-        </article>
-      </section>
-    </div>
+      <p className={styles.description}>Interesting things I want to check out!</p>
+      <div className={styles.columns}>
+        <Column title="Books" icon="book" />
+        <Column title="Movies" icon="film" />
+        <Column title="Games" icon="gamepad" />
+      </div>
+    </section>
   );
 };
 
 export default List;
+
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import App from './App';
 
 import './styles/normalize.scss';
 import './styles/global.scss';
+import 'font-awesome/css/font-awesome.min.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
