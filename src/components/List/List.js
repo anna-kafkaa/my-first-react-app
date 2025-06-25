98d3d1b678b25a0bfa21576954380cfb621d8ea8
Anna Kafka <anna.kafka933@gmail.com>
2025-06-25 17:35:41 +0200

    Add local SCSS modules to SearchForm and List components

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,21 +1,25 @@
+import styles from './List.module.scss';
+
 const List = () => {
   return (
-    <div>
-        <header>
-            <h2>Things to do<span>soon!</span></h2>
-        </header>
-        <p>Interesting things I want to check out</p>
-        <section>
-            <article>
-                <h2>Books</h2>
-            </article>
-            <article>
-                <h2>Movies</h2>
-            </article>
-            <article>
-                <h2>Games</h2>
-            </article>
-        </section>
+    <div className={styles.list}>
+      <header className={styles.header}>
+        <h2 className={styles.title}>
+          Things to do <span>soon!</span>
+        </h2>
+      </header>
+      <p className={styles.description}>Interesting things I want to check out</p>
+      <section className={styles.columns}>
+        <article>
+          <h2>Books</h2>
+        </article>
+        <article>
+          <h2>Movies</h2>
+        </article>
+        <article>
+          <h2>Games</h2>
+        </article>
+      </section>
     </div>
   );
 };
