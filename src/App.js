11dbe92740f4de267ae8896f7172b91d8103e9d2
Anna Kafka <anna.kafka933@gmail.com>
2025-06-25 19:04:58 +0200

    Add Container component to wrap page content using children prop

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import Hero from './components/Hero/Hero.js';
-import SearchForm from './components/SearchForm/SearchForm.js';
-import List from './components/List/List.js';
+import Hero from './components/Hero/Hero';
+import SearchForm from './components/SearchForm/SearchForm';
+import List from './components/List/List';
+import Container from './components/Container/Container';
 
 const App = () => {
   return (
-    <div>
+    <Container>
       <Hero />
       <SearchForm />
       <List />
-    </div>
+    </Container>
   );
 };
 
-export default App; 
+export default App;
+
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,13 +1,13 @@
-import styles from './SearchForm.module.scss';
+import styles from './SearchForm.module.scss'
 import TextInput from '../TextInput/TextInput';
 
-
 const SearchForm = () => {
-  return (
-    <form className={styles.searchForm}>
-      <TextInput placeholder="Search..." />
-    </form>
-  );
+    return (
+        <form className={styles.searchForm}>
+            <TextInput placeholder="Search..." />
+            <button className={styles.button}>Search</button>
+        </form>
+    );
 };
 
 export default SearchForm;
