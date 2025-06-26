b02e8557d9fecaeebdd4c99d05f10103629dcbfb
Anna Kafka <anna.kafka933@gmail.com>
2025-06-26 09:47:59 +0200

    Render columns dynamically using map()

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,28 +1,32 @@
+import React, { useState } from 'react';
 import styles from './List.module.scss';
 import Column from '../Column/Column';
 
 const List = () => {
-  const columns = [
+  const [columns, setColumns] = useState([
     { id: 1, title: 'Books', icon: 'book' },
     { id: 2, title: 'Movies', icon: 'film' },
     { id: 3, title: 'Games', icon: 'gamepad' },
-  ];
+  ]);
+
+  setTimeout(() => {
+    setColumns(prevColumns => [
+      ...prevColumns,
+      { id: 4, title: 'Test column' }
+    ]);
+  }, 2000);
 
   return (
-    <section className={styles.list}>
-      <header className={styles.header}>
-        <h2 className={styles.title}>Things to do <span>soon!</span></h2>
-      </header>
-      <p className={styles.description}>Interesting things I want to check out!</p>
-      <div className={styles.columns}>
-        {columns.map(column => (
-          <Column key={column.id} title={column.title} icon={column.icon} />
-        ))}
-      </div>
+    <section className={styles.columns}>
+      {columns.map(column => (
+        <Column key={column.id} title={column.title} icon={column.icon} />
+      ))}
     </section>
   );
 };
 
 export default List;
+
+
 
 
