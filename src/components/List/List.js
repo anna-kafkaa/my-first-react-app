a354fb39806b013f419285750a4182ec4488bb3c
Anna Kafka <anna.kafka933@gmail.com>
2025-06-26 11:02:00 +0200

    Add useState and useEffect to dynamically render columns

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,31 +1,44 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import styles from './List.module.scss';
 import Column from '../Column/Column';
 
 const List = () => {
-
   const [columns, setColumns] = useState([
-      { id: 1, title: 'Books', icon: 'book' },
-      { id: 2, title: 'Movies', icon: 'film' },
-      { id: 3, title: 'Games', icon: 'gamepad' }
+    { id: 1, title: 'Books', icon: 'book' },
+    { id: 2, title: 'Movies', icon: 'film' },
+    { id: 3, title: 'Games', icon: 'gamepad' },
   ]);
 
-  setTimeout(() => {
-    setColumns([...columns, { id: 4, title: 'Test column'}]);
-  }, 2000);
-
-};
+  useEffect(() => {
+    setTimeout(() => {
+      setColumns(prevColumns => [
+        ...prevColumns,
+        { id: 4, title: 'Test column' }
+      ]);
+    }, 2000);
+  }, []);
 
   return (
-    <section className={styles.columns}>
-      {columns.map(column => (
-        <Column key={column.id} title={column.title} icon={column.icon} />
-      ))}
+    <section className={styles.list}>
+      <header className={styles.header}>
+        <h2 className={styles.title}>Things to do <span>soon!</span></h2>
+      </header>
+      <p className={styles.description}>Interesting things I want to check out!</p>
+      <div className={styles.columns}>
+        {columns.map(column => (
+          <Column
+            key={column.id}
+            title={column.title}
+            icon={column.icon}
+          />
+        ))}
+      </div>
     </section>
   );
 };
 
 export default List;
+
 
 
 
